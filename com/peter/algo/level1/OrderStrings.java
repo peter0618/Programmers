13202bbafe5ed17e3065f62ab380eaf37ae3f280
peter0618 <peterchoi0618@gmail.com>
2019-10-19 02:43:47 +0900

    [수정] 문자열 내 마음대로 정렬하기 if else 문 조건을 줄일 수 있어서 리펙토링

--- com/peter/algo/level1/OrderStrings.java
+++ com/peter/algo/level1/OrderStrings.java
@@ -9,7 +9,7 @@ public class OrderStrings {
         OrderStrings orderStrings = new OrderStrings();
 //        String[] strings = { "sun", "bed", "car" };
 //        int n = 1;
-        
+
         String[] strings = { "abce", "abcd", "cdx" };
         int n = 2;
 
@@ -18,20 +18,25 @@ public class OrderStrings {
     }
 
     public String[] solution(String[] strings, int n) {
-        
+
         Arrays.sort(strings, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                if (s1.charAt(n) > s2.charAt(n)) {
-                    return 1;
-                } else if (s1.charAt(n) == s2.charAt(n)) {
+//                if (s1.charAt(n) > s2.charAt(n)) {
+//                    return 1;
+//                } else if (s1.charAt(n) == s2.charAt(n)) {
+//                    return s1.compareTo(s2);
+//                } else {
+//                    return -1;
+//                }
+                if (s1.charAt(n) == s2.charAt(n)) {
                     return s1.compareTo(s2);
                 } else {
-                    return -1;
+                    return s1.charAt(n) - s2.charAt(n);
                 }
             };
         });
-        
+
         return strings;
     }
 }
