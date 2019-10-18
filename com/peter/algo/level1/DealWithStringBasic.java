2469f0bde33d35b4866ed4a5aaa35030d6dcd3b0
peter0618 <peterchoi0618@gmail.com>
2019-10-19 02:57:03 +0900

    문자열 다루기 기본

---
+++ com/peter/algo/level1/DealWithStringBasic.java
@@ -0,0 +1,27 @@
+package com.peter.algo.level1;
+
+public class DealWithStringBasic {
+    public static void main(String[] args) {
+        DealWithStringBasic basic = new DealWithStringBasic();
+        
+        System.out.println(basic.solution("a234"));
+        System.out.println(basic.solution("1234"));
+    }
+    
+    /*
+     * 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수.
+     */
+    public boolean solution(String s) {
+
+        if((s.length() != 4) && (s.length() != 6)) {
+            return false;
+        } else {
+            try {
+                Integer.parseInt(s);    
+            } catch(NumberFormatException e) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
