f4a2f92e31ed3aab1161dac62581469fbaa13567
peter0618 <peterchoi0618@gmail.com>
2019-10-22 00:49:39 +0900

    [수정]약수의 합 - (n/2) 과 n 사이에 약수가 없으므로, (n/2)까지만 약수를 확인하면 된다.   (연산 횟수를 절반으로 줄이는 효과)

--- com/peter/algo/level1/SumOfDivisor.java
+++ com/peter/algo/level1/SumOfDivisor.java
@@ -2,8 +2,8 @@ package com.peter.algo.level1;
 
 public class SumOfDivisor {
     public static void main(String[] args) {
-        System.out.println(solution(12));
-        System.out.println(solution(5));
+        System.out.println(solution2(12));
+        System.out.println(solution2(5));
     }
     
     public static int solution(int n) {
@@ -14,6 +14,16 @@ public class SumOfDivisor {
             }
         }
         return sum;
-    }   
+    }
+    
+    public static int solution2(int n) {
+        int sum = 0;
+        for(int i = 1 ; i <= (n/2) ; i++) {
+            if(n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum + n;
+    }
 
 }
