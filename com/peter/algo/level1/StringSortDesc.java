2e31729e672b95bc4362119ad4dec42811b7bb56
peter0618 <peterchoi0618@gmail.com>
2019-10-21 16:24:23 +0900

    [수정]문자열 내림차순으로 배치하기 Collections.reverseOrder() 를 사용하면 역순(내림차순) 정렬 Comparator를 가져올 수 있다.

--- com/peter/algo/level1/StringSortDesc.java
+++ com/peter/algo/level1/StringSortDesc.java
@@ -1,7 +1,7 @@
 package com.peter.algo.level1;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class StringSortDesc {
@@ -13,12 +13,13 @@ public class StringSortDesc {
     
     public static String solution(String s) {
         Character[] cArr = s.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
-        Arrays.sort(cArr, new Comparator<Character>() {
-            @Override
-            public int compare(Character c1, Character c2) {
-                return c2.compareTo(c1);            // 문자 정렬 순서 반대로 변경 (c1.comp(c2)가 default)
-            }
-        });
+//        Arrays.sort(cArr, new Comparator<Character>() {
+//            @Override
+//            public int compare(Character c1, Character c2) {
+//                return c2.compareTo(c1);            // 문자 정렬 순서 반대로 변경 (c1.comp(c2)가 default)
+//            }
+//        });
+        Arrays.sort(cArr, Collections.reverseOrder() );
         return Arrays.stream(cArr).map(String::valueOf).collect(Collectors.joining());
     }
 }
