d05cf8337e8fec42f5c8dd1d196d0d74fc870468
peter0618 <peterchoi0618@gmail.com>
2019-10-22 16:15:17 +0900

    [풀이]예산

---
+++ com/peter/algo/level1/Budget.java
@@ -0,0 +1,30 @@
+package com.peter.algo.level1;
+
+import java.util.Arrays;
+
+public class Budget {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1,3,2,5,4}, 9));
+        System.out.println(solution(new int[]{2,2,3,3},  10));
+        System.out.println(solution(new int[]{2,3,2,4},  7));
+        System.out.println(solution(new int[]{1,2,3,4},  1000));
+    }
+    
+    public static int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int sum = 0;
+        int idx = 0;
+        // d[0] + d[1] + ... +d[k] <= budget 이 되는 최대의 k를 찾는다.
+        while(sum < budget) {
+            sum += d[idx++];
+            if(idx >= d.length) {   // d의 모든 원소의 sum이 budget보다 작은 경우 d[idx++]에서 OutOfBound Exception 발생하므로 회피
+                break;
+            }
+        }
+        if(sum > budget) {
+            idx--;
+        }
+        
+        return idx;
+    }
+}
