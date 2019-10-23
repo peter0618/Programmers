b3183323bf2de42960bf3d7a70540737673eb517
peter0618 <peterchoi0618@gmail.com>
2019-10-23 13:39:38 +0900

    [풀이] 제일 작은 수 제거하기

---
+++ com/peter/algo/level1/DeleteMin.java
@@ -0,0 +1,31 @@
+package com.peter.algo.level1;
+
+import java.util.Arrays;
+
+public class DeleteMin {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {4,3,2,1}))); // [4,3,2]
+        System.out.println(Arrays.toString(solution(new int[] {10})));      // [-1]
+    }
+    
+    public static int[] solution(int[] arr) {
+        if(arr.length == 1) {
+            return new int[] {-1};
+        }
+        
+        int min = arr[0];
+        for(int i = 1; i <arr.length ; i++) {
+            min = Math.min(min, arr[i]);
+        }
+        
+        int[] answer = new int[arr.length-1];
+        int idx = 0;
+        
+        for(int num : arr) {
+            if(num != min) {
+                answer[idx++] = num;
+            }
+        }
+        return answer;
+    }
+}
